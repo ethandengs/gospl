ff5be8a743f64bd6608df1580fdc837fec90b316
Ethan Deng <ethandengs@gmail.com>
2025-04-17 18:06:33 +0800

    try 2

--- web-app/src/app/not-found.tsx
+++ web-app/src/app/not-found.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link'
-
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h2 className="text-xl font-semibold">404 - Page Not Found</h2>
-      <p className="mt-4">The page you're looking for doesn't exist.</p>
+      <p className="mt-4">The page you&apos;re looking for doesn&apos;t exist.</p>
     </div>
   )
 } 
